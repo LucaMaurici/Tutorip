588d9672d3649dc5f57fb3f8e1da6e651473e89a
a-vitale <63303554+a-vitale@users.noreply.github.com>
2020-06-04 11:14:52 +0200

    modifiche: -Inserimento della SignUp page; -Inserimento della SignUpTutorPage; -Gestione della CircleImage; -Installazione del NuGet "Xam.Plugins.Forms.ImageCircle";

---
+++ Tutorip/Views/SignUpTutorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tutorip.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpTutorPage : ContentPage
+    {
+        public SignUpTutorPage()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator Button(SignUpTutorPage v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void aggiungiImage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
