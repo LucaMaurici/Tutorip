3f2570e652ee5b87c3b59424d61bbd02f413bdcd
LorenzoMattia <63303421+LorenzoMattia@users.noreply.github.com>
2020-06-07 13:01:25 +0200

    List view nel menu

---
+++ Tutorip/Models/ElementoMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorip.Models
+{
+    public class ElementoMenu
+    {
+        public String pathImmagine { get; set; }
+        public String testo { get; set; }
+        public ElementoMenu(String pathImmagine, String testo)
+        {
+            this.pathImmagine = pathImmagine;
+            this.testo = testo;
+        }
+    }
+}
